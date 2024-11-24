6bd08b1f766fbf4940f4c3bdab2ef1f96e589dc9
Marcus Arnström <7047150+JazzMasta@users.noreply.github.com>
2024-11-24 18:48:38 +0100

    Create main.tf

---
+++ azure/main.tf
@@ -0,0 +1,16 @@
+
+//Template 
+
+terraform {
+  required_providers: {
+    azurerm: {
+      source: "hashicorp/azurerm"
+      version: "~"
+    }
+  }
+}
+
+provider "azurerm" {
+  features: {}
+}
+
